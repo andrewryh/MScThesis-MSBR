ad077357a7c3d13d5bac9c4135ecbd2e9d248c1a
andrewryh <andrewryh@gmail.com>
2018-03-30 08:40:18 -0500

    Chapter 4 (results) finished

---
+++ serpent/thermal_coeff/core_dep.m
@@ -0,0 +1,105 @@
+
+ZAI = [
+666
+0
+];
+
+NAMES = [
+'lost            '
+'total           '
+];
+
+iLOST    = 1;
+iTOT     = 2;
+
+MAT_fuel_VOLUME = [ 5.51573E+06 5.51573E+06 ];
+
+MAT_fuel_BURNUP = [ 0.00000E+00 8.43463E-01 ];
+
+MAT_fuel_ADENS = [
+0.00000E+00 0.00000E+00 % lost data
+7.77767E-02 7.77802E-02 % total
+];
+
+MAT_fuel_MDENS = [
+0.00000E+00 0.00000E+00 % lost data
+3.26636E+00 3.26636E+00 % total
+];
+
+MAT_fuel_A = [
+0.00000E+00 0.00000E+00 % lost data
+5.85536E+13 3.71394E+20 % total
+];
+
+MAT_fuel_H = [
+0.00000E+00 0.00000E+00 % lost data
+4.60858E+01 1.13874E+08 % total
+];
+
+MAT_fuel_SF = [
+0.00000E+00 0.00000E+00 % lost data
+4.45749E-01 3.98669E+00 % total
+];
+
+MAT_fuel_GSRC = [
+0.00000E+00 0.00000E+00 % lost data
+4.95115E+12 3.52696E+20 % total
+];
+
+MAT_fuel_ING_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+2.99193E+06 8.28661E+10 % total
+];
+
+MAT_fuel_INH_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+5.65312E+08 6.76847E+10 % total
+];
+
+TOT_VOLUME = [ 5.51573E+06 5.51573E+06 ];
+
+TOT_BURNUP = [ 0.00000E+00 8.43463E-01 ];
+
+TOT_ADENS = [
+0.00000E+00 0.00000E+00 % lost data
+7.77767E-02 7.77802E-02 % total
+];
+
+TOT_MASS = [
+0.00000E+00 0.00000E+00 % lost data
+1.80164E+07 1.80164E+07 % total
+];
+
+TOT_A = [
+0.00000E+00 0.00000E+00 % lost data
+5.85536E+13 3.71394E+20 % total
+];
+
+TOT_H = [
+0.00000E+00 0.00000E+00 % lost data
+4.60858E+01 1.13874E+08 % total
+];
+
+TOT_SF = [
+0.00000E+00 0.00000E+00 % lost data
+4.45749E-01 3.98669E+00 % total
+];
+
+TOT_GSRC = [
+0.00000E+00 0.00000E+00 % lost data
+4.95115E+12 3.52696E+20 % total
+];
+
+TOT_ING_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+2.99193E+06 8.28661E+10 % total
+];
+
+TOT_INH_TOX = [
+0.00000E+00 0.00000E+00 % lost data
+5.65312E+08 6.76847E+10 % total
+];
+
+BU = [ 0.00000E+00 8.42515E-01 ];
+
+DAYS = [ 0.00000E+00 3.00000E+00 ];
